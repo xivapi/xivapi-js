b467f40a880d9f376422c672f14b9795b2ad65fd
Kimon Kar <kimon.kar@gmail.com>
2019-01-04 22:48:37 +0200

    Basic progress

---
+++ template.js
@@ -0,0 +1,24 @@
+// https://xivapi.com/docs/Welcome#section-4
+const req = require('request-promise-native')
+
+/*TODO: columns, tags,
+*/
+module.export = function(string, params) {
+	req({
+		uri: this.endpoint + '',
+		qs: {
+			key: 				this.apikey,
+			language: 	this.options.lang,
+			snake_case: this.options.snakecase ? 1 : 0,
+
+			
+		},
+		json: true
+	})
+		.then((res) => {
+
+		})
+		.catch((err) => {
+
+		})
+}
